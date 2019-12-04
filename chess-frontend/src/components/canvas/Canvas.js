420318aa3e21774bc81e41e9c8539c5ef08fdb3d
cokepizza <cokepizza3@gmail.com>
2019-12-04 20:43:54 +0900

    [frontend] 체스판 구현 및 state 결정

--- chess-frontend/src/components/canvas/Canvas.js
+++ chess-frontend/src/components/canvas/Canvas.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const chessState = 
+[
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+];
+
 const CanvasFrameBlock = styled.div`
     height: 800px;
     width: 100%;
@@ -11,30 +23,56 @@ const CanvasFrameBlock = styled.div`
 `;
 
 const CanvasBlock = styled.div`
-    width : 720px;
-    height: 720px;
+    position: relative;
+    border: 1px solid black;
     background-color: blue;
 `;
 
+const CanvasRowBlock = styled.div`
+    display: flex;
+`;
+
 const CanvasCellBlock = styled.div`
+    display: flex;
+    cursor: pointer;
+    justify-content: center;
+    align-items: center;
     width: 90px;
     height: 90px;
-    
-    &::after {
-        border-right: 1px solid black;
+    border: 1px solid black;
+    &:hover {
+        border: 1px solid pink;
     }
 
-    &::before {
-        border-bottom: 1px solid black;
+    &:active {
+        background-color: skyblue;
     }
+`
 
-`;
+const CanvasContent = () => {
+    return  (
+        <>
+            {
+                chessState.map(rowState => (
+                    <CanvasRowBlock>
+                        {rowState.map(cell => (
+                            <CanvasCellBlock>
+                                {cell}
+                            </CanvasCellBlock>
+                        ))
+                        }
+                    </CanvasRowBlock>
+                ))
+            }
+        </>
+    )
+}
 
 const Canvas = props => {
     return (
         <CanvasFrameBlock>
             <CanvasBlock>
-                <CanvasCellBlock />
+                <CanvasContent />
             </CanvasBlock>
         </CanvasFrameBlock>
     )
