4baccba741e7e99739aa1dff78e3aa6d555845ba
cokepizza <cokepizza3@gmail.com>
2019-12-09 14:05:12 +0900

    [frontend, backend] 백엔드, 프론트엔드 웹소켓 라이브러리 추가 (koa-socket-2, socket.io-client) v2.socket.io 도입. room 추가 & broadcasting에 유동적인 라이브러리

--- chess-backend/src/main.js
+++ chess-backend/src/main.js
@@ -4,32 +4,47 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import ws from 'ws';
-import { WSASERVICE_NOT_FOUND } from 'constants';
+import Io from 'koa-socket-2';
 
 const app = new Koa();
+const io = new Io();
 const router = new Router();
 const { PORT } = process.env;
 
-const wws = new ws.Server({ port: 4000 });
-
-let count = 0;
-wws.on('connection', ws => {
-    console.dir('connection complete');
-    ws.on('message', message => {
-        console.log(`message: ${message}`);
-    })
-    setInterval(() => {
-        console.dir('we send');
-        ws.send(`count: ${count++}`);
-    }, 1000)
-})
+// let count = 0;
+// const wws = new ws.Server({ port: 4000 });
+// wws.on('connection', ws => {
+//     console.dir('connection complete');
+//     ws.on('message', message => {
+//         console.log(`message: ${message}`);
+//     })
+//     setInterval(() => {
+//         console.dir('we send');
+//         ws.send(`count: ${count++}`);
+//     }, 1000)
+// })
 
 
 app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-// const port = PORT || 4000;
-app.listen(5000, () => {
+io.attach(app);
+
+let count = 0;
+io.on('connection', socket => {
+    console.dir('connect~~');
+    setInterval(() => {
+        socket.emit('message', count++);
+    }, 1000);
+    // socket.on('message', msg => {
+    //     console.dir(msg);
+    // })
+});
+
+
+
+const port = PORT || 4000;
+app.listen(4000, () => {
     console.log('Listening to port 4000');
-})
+});
--- chess-frontend/src/modules/chat.js
+++ chess-frontend/src/modules/chat.js
@@ -1,25 +1,29 @@
 import { createAction, handleActions } from 'redux-actions';
 import { eventChannel } from 'redux-saga';
 import { takeEvery, put, call, take } from 'redux-saga/effects';
+import SocketIo from 'socket.io-client';
 
 const INITIALIZE_WEBSOCKET = 'chat/INITIALIZE_WEBSOCKET';
 const WEBSOCKET_ONMESSAGE = 'chat/WEBSOCKET_ONMESSAGE';
 
 export const initializeWebsocket = createAction(INITIALIZE_WEBSOCKET);
 
-function* createEventChannel(socket) {
+function* createEventChannel(io) {
     return eventChannel(emit => {
-        socket.onmessage = message => emit(message.data);
+        // socket.onmessage = message => emit(message.data);
+        io.on('message', message => emit(message));
+
         return () => {
-            socket.close();
+            io.close();
         }
     })
 }
 
 function* initializeWebsocketSaga () {
-    const socket = new WebSocket('ws://localhost:4000', 'protocol');
+    // const socket = new WebSocket('ws://localhost:4000', 'protocol');
+    const io = SocketIo('ws://localhost:4000');
     console.dir('소켓 연결했을까');
-    const channel = yield call(createEventChannel, socket);
+    const channel = yield call(createEventChannel, io);
     while(true) {
         const message = yield take(channel);
         yield put({ type: WEBSOCKET_ONMESSAGE, message});
