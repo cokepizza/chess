2f82621394229429257217ddac11bfe19fa6e2d5
cokepizza <cokepizza3@gmail.com>
2019-12-10 09:52:19 +0900

    [backend] express server request hang 문제 해결 res.end()를 추가해야함

--- chess-backend/src/api/chat/chatCtrl.js
+++ chess-backend/src/api/chat/chatCtrl.js
@@ -3,19 +3,5 @@ export const sendMessage = (req, res, next) => {
     const { message } = req.body;
     io.emit('message', message);
     console.dir(`message received : ${message}`);
-    // console.dir(ctx);
-    // ctx.socket.emit('haha');
-    
-    // console.dir(ctx.socket);
-    // ctx.body = ctx.socket;
-    // ctx.socket.broadcast.emit('message', message);
-    // const { socket } = ctx.state;
-    
-
-    // console.dir(socket);
-    // console.dir(io);
-    // console.dir(message);
-    // console.dir(app._io);
-    // socket.broadcast.emit('message', message);
-    // socket.emit('message', message);
+    res.status(202).end();
 };
