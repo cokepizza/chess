3103770f31c9f5f950941cd58ed9f800318f3adc
cokepizza <cokepizza3@gmail.com>
2019-12-11 20:42:47 +0900

    [backend] socket 접속 순서에 따라 플레이어 결정 로직 구현 중 플레이어 차례에서만 기능이 동작하도록 frontend 업데이트할 예정

--- chess-backend/src/socket.js
+++ chess-backend/src/socket.js
@@ -2,11 +2,26 @@ import SocketIO from 'socket.io';
 
 export default (server, app, sessionMiddleware) => {
     const io = SocketIO(server);
+    let counter = 0;
+    const player = new Map();
+
     app.set('io', io);
+    app.set('player', player);
+
     io.use((socket, next) => {
         sessionMiddleware(socket.request, socket.request.res, next);
     })
     io.on('connection', socket => {
-        console.dir('welcome~');
+        console.dir(socket.id);
+        if(counter === 0) {
+            player.set('black', socket.id);
+        } else if(counter === 1) {
+            player.set('white', socket.id);
+        } else {
+            player.set('spectator', socket.id);
+        }
+        ++counter;
+        
+        console.dir(`welcome : ${socket.id}`);
     })
 };
