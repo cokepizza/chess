abb26d080af220a759bf543e1590e87ac10a1d41
yeonggyu_lim <yeonggyu_lim@tmax.co.kr>
2019-12-12 21:39:05 +0900

    [frontend] [Record] Record draft 1 - static한 값으로 지정

--- chess-frontend/src/components/content/Record.js
+++ chess-frontend/src/components/content/Record.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
 const RecordBlock = styled.div`
-    position: relative;
     border: 1px solid black;
-    background-color: blue;
+    background-color: white;
     height: 50%;
+    overflow-y: auto;
 `;
 
+const RecordItem = styled.div`
+    padding: 0.3rem;
+    display: flex;
+    &:nth-child(even) {
+        background: #f2f3f4;
+    }
+    & + & {
+        border-top: 1px solid #dee2e6;
+    }
+`;
 
+const Record = (  ) => {
+    const ref = useRef();
+    const records = [ '1. e4 h5', '2. g4 e5', '3. h4 f6', '4. Rh3 d5', '5. Ke2 hxg4', '6. Nf3 Qd7', '7. Ke2 Qb6', '8. Qa4 c5'];
+    useEffect(() => {
+        ref.current.scrollTop = ref.current.scrollHeight;
+    }, [records]);
 
-const Record = () => {
     return (
-        <RecordBlock>
-            Record
-        </RecordBlock>
+        <>
+            <RecordBlock ref={ref}>
+                {records.map(record => (
+                    <RecordItem>{record}</RecordItem>
+                ))}
+            </RecordBlock>
+        </>
     );
 };
 
