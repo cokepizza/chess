49879e5517229d8bacc776210569b79433567c61
yeonggyu_lim <yeonggyu_lim@tmax.co.kr>
2019-12-12 19:43:40 +0900

    [frontend] [Record] 레코드 구조를 페이지와 컨테이너 사용하게 변경

--- chess-frontend/src/App.js
+++ chess-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MainPage from './pages/MainPage';
 import ChatPage from './pages/ChatPage';
+import RecordPage from './pages/RecordPage';
 import { Route } from 'react-router-dom';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <Route component={MainPage} path='/' exact />
       <Route component={ChatPage} path='/chat' exact />
+      <Route component={RecordPage} path='/record' exact />
     </>
   )
 }
---
+++ chess-frontend/src/pages/RecordPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RecordContainer from '../containers/content/RecordContainer'
+
+const RecordPage = () => {
+    return (
+        <RecordContainer />
+    );
+};
+
+export default RecordPage;
