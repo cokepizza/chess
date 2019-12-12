993a7b90d430aa9eda9881aa6b4240e9fa470d6b
Hoseung_Shin <nwdt3309@gmail.com>
2019-12-12 22:50:30 +0900

    [feat]  챗창 그레이 스타일로 변경

--- chess-frontend/src/components/content/Chat.js
+++ chess-frontend/src/components/content/Chat.js
@@ -1,14 +1,34 @@
 import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
+const ChatSubmitBlock = styled.form`
+`;
+
 const ChatFrameBlock = styled.div`
-    width: 300px;
+    margin: 10px 10px 10px 10px;
+    border : 3px groove gray;
+    border-style: outset;
+    border-top-right-radius: 10px;
+    border-top-left-radius: 10px;
+    width: 450px;
+    box-shadow: 5px 5px 5px;
 `;
 
 const ChatBlock = styled.div`
-    height: 500px;
-    border: 1px solid black;
+    height: 300px;
+    margin: 10px 10px 10px 10px;
+    border : none;
     overflow-y: scroll;
+    ::-webkit-scrollbar {
+        width: 20px;
+    }
+    ::-webkit-scrollbar-track {
+        color: transparent;
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: lightgray;
+        border-radius: 10px;
+    }
 `;
 
 const ChatFormBlock = styled.div`
@@ -19,11 +39,44 @@ const ChatFormBlock = styled.div`
 
 const ChatInputBlock = styled.input`
     outline: none;
+    align-items : center;
+    color: black;
+    font-size: 15px;
+    margin : 10px 10px 10px 10px;
     width: 80%;
+    height : 30px;
+    border: 3px solid lightgray;
+    transition: all 0.3s ease .1s;
+    &:hover, &:focus {
+        border: 3px solid gray;
+    } 
 `;
 
 const ChatButtonBlock = styled.button`
     width: 20%;
+    font-size: 15px;
+    margin : 10px 10px 10px 10px;
+    background-color: transparent;
+    border: 3px solid lightgray;
+    transition: all 0.3s ease .1s;
+    &:hover, &:focus {
+        border: 3px solid gray;
+    } 
+`;
+
+const MessageDiv = styled.div`
+    padding : 5px;
+    min-height : 20px;
+    font-size : 15px;
+    :nth-child(even) {
+        color : black ;
+    }
+    :nth-child(odd) {
+        color : blue ;
+    }
+    & + & {
+        border-top :1px solid lightgrey;
+    }
 `;
 
 const Chat = ({ messages, onSubmit, onChange, text }) => {
@@ -35,26 +88,26 @@ const Chat = ({ messages, onSubmit, onChange, text }) => {
 
     return (
         <>
-            <form onSubmit={onSubmit}>
+            <ChatSubmitBlock onSubmit={onSubmit}>
                 <ChatFrameBlock>
                     <ChatBlock ref={ref}>
                         {messages.map(message => (
-                            <div>{message}</div>
+                            <MessageDiv>{message}</MessageDiv>
                         ))}
                     </ChatBlock>
                     <ChatFormBlock>
                         <ChatInputBlock
                             onChange={onChange}
-                            value ={text}
+                            value={text}
                         />
                         <ChatButtonBlock
                             onSubmit={onSubmit}
                         >
-                            보내기
+                            Send
                         </ ChatButtonBlock>
                     </ChatFormBlock>
                 </ChatFrameBlock>
-            </form>
+            </ChatSubmitBlock>
         </>
     )
 };
