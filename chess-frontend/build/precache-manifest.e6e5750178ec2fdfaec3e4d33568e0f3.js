1af13e483de9b5ebadffc84f8f4b43f252ba2124
cokepizza <cokepizza3@gmail.com>
2019-12-29 17:11:32 +0900

    [frontend] ec2 프리티어 메모리 부족으로 git 에 빌드까지해서 올린 후 배포하는 것으로 변경

---
+++ chess-frontend/build/precache-manifest.e6e5750178ec2fdfaec3e4d33568e0f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afaf0ae6e11889a430632352db76c9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "283d89f0233fd77835c6",
+    "url": "/static/css/main.306e2ed7.chunk.css"
+  },
+  {
+    "revision": "d84f6cf4fb25512bbf44",
+    "url": "/static/js/2.fb3b08fc.chunk.js"
+  },
+  {
+    "revision": "283d89f0233fd77835c6",
+    "url": "/static/js/main.eefdb69b.chunk.js"
+  },
+  {
+    "revision": "091b2053e67b041b4c79",
+    "url": "/static/js/runtime-main.b30cecd2.js"
+  }
+]);
