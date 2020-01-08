6cb759a5bf0444273f8ea5cb2f78e5dd642cadf2
Hoseung_Shin <nwdt3309@gmail.com>
2020-01-08 09:41:33 +0900

    [feat] 커뮤니티 css

--- chess-frontend/src/components/community/CommunityTableRow.js
+++ chess-frontend/src/components/community/CommunityTableRow.js
@@ -4,12 +4,20 @@ import styled from 'styled-components';
 const CommunityTableRowBlock = styled.tr`
     width: 100%;
     height: 100px;
+    transition-duration: 0.2s;
     &:nth-child(odd) {
         background: #F7F6F4;
     }
+    &:hover {
+        box-shadow: 0 0 5px 5px #ADD8E6;
+        background: #ADD8E6;
+        &:active {
+            box-shadow: 0 0 10px 10px #ADD8E6;
+        }
+    }  
 `;
 
-const CommunityTableRow = ( {title, count, comments, time}) => {
+const CommunityTableRow = ({ title, count, comments, time }) => {
     return (
         <CommunityTableRowBlock>
             <td>{title}</td>
