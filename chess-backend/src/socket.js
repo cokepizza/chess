6f879bb63aadfd097d4d71cfe05c250e10009a9c
cokepizza <cokepizza3@gmail.com>
2020-01-13 00:45:35 +0900

    [backend] 소켓 문제 해결 중

--- chess-backend/src/socket.js
+++ chess-backend/src/socket.js
@@ -29,43 +29,40 @@ const connectGame = (app, io, socket, key) => {
     if(game._participant.has(sessionId)) {
         const socketSet = game._participant.get(sessionId);
         socketSet.add(socket.id);
-        
-        if(socketSet.size === 5) {
-            if(!game._white) {
-                game.white = nickname;
-                game._white = sessionId;
-            } else if(game._white !== sessionId && !game._black) {
-                game.black = nickname;
-                game._black = sessionId;
-            }
-
-            if(game._white && game._black) {
-                if(sessionId === game._white || sessionId === game._black) {
-                    if(game._participant.has(game._white) && game._participant.has(game._black)) {
-                        const record = app.get('record').get(key);
-                        record._start(game.order);
-                        game.start = true;
-                    };
-                }
-            }
-            
-            io.of('/game').to(key).emit('message', {
-                type: 'initialize',
-                ...instanceSanitizer(game),
-            });
-    
-            io.of('/games').emit('message', {
-                type: 'initialize',
-                games: [...gameMap.values()],
-            });
-        }
     } else {
         //  first socket only serve
         game.participant.push(nickname);
         game._participant.set(sessionId, new Set([socket.id]));
+
+        if(!game._white) {
+            game.white = nickname;
+            game._white = sessionId;
+        } else if(game._white !== sessionId && !game._black) {
+            game.black = nickname;
+            game._black = sessionId;
+        }
+
+        if(game._white && game._black) {
+            if(sessionId === game._white || sessionId === game._black) {
+                if(game._participant.has(game._white) && game._participant.has(game._black)) {
+                    const record = app.get('record').get(key);
+                    record._start(game.order);
+                    game.start = true;
+                };
+            }
+        }
+        
+        io.of('/game').to(key).emit('message', {
+            type: 'initialize',
+            ...instanceSanitizer(game),
+        });
+
+        io.of('/games').emit('message', {
+            type: 'initialize',
+            games: [...gameMap.values()],
+        });
     }
     console.dir(game);
-
 }
 
 const disconnectGame = (app, io, socket, key) => {
