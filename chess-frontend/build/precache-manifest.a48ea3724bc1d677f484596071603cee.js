c4be5ebc10e13e840b327afb348851e8ec7d2390
cokepizza <cokepizza3@gmail.com>
2020-02-16 21:11:16 +0900

    [frontend] rebuild

---
+++ chess-frontend/build/precache-manifest.a48ea3724bc1d677f484596071603cee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9017b5d0bb13b605419a7cf9e00afe13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26434456bb3075402c5f",
+    "url": "/static/css/main.480f3d05.chunk.css"
+  },
+  {
+    "revision": "9201cf692660a8bb1dbb",
+    "url": "/static/js/2.3bffdb75.chunk.js"
+  },
+  {
+    "revision": "26434456bb3075402c5f",
+    "url": "/static/js/main.d450a57d.chunk.js"
+  },
+  {
+    "revision": "091b2053e67b041b4c79",
+    "url": "/static/js/runtime-main.b30cecd2.js"
+  },
+  {
+    "revision": "78f675e7aeaad79d813b8bf516cc1d39",
+    "url": "/static/media/ranking.78f675e7.svg"
+  },
+  {
+    "revision": "390a617d691fa067c2364ec7f6097a9b",
+    "url": "/static/media/spinner.390a617d.svg"
+  }
+]);
