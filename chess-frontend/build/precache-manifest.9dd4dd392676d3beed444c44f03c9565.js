c3ae7decb52071d7cd4a5448e3c1abedd8cfae45
cokepizza <cokepizza3@gmail.com>
2020-02-27 13:42:37 +0900

    [frontend] build

---
+++ chess-frontend/build/precache-manifest.9dd4dd392676d3beed444c44f03c9565.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0701d462c351da47b87279a8efafe75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51c604cb85c40709de6a",
+    "url": "/static/css/2.d15af080.chunk.css"
+  },
+  {
+    "revision": "1f32b398e8609776b116",
+    "url": "/static/css/main.480f3d05.chunk.css"
+  },
+  {
+    "revision": "51c604cb85c40709de6a",
+    "url": "/static/js/2.044908b7.chunk.js"
+  },
+  {
+    "revision": "1f32b398e8609776b116",
+    "url": "/static/js/main.35850373.chunk.js"
+  },
+  {
+    "revision": "091b2053e67b041b4c79",
+    "url": "/static/js/runtime-main.b30cecd2.js"
+  },
+  {
+    "revision": "78f675e7aeaad79d813b8bf516cc1d39",
+    "url": "/static/media/ranking.78f675e7.svg"
+  },
+  {
+    "revision": "390a617d691fa067c2364ec7f6097a9b",
+    "url": "/static/media/spinner.390a617d.svg"
+  }
+]);
--- chess-frontend/build/service-worker.js
+++ chess-frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79bc07927b67c1b73082f224be28f04b.js"
+  "/precache-manifest.9dd4dd392676d3beed444c44f03c9565.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
